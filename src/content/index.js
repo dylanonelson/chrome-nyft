278dd25991dcc1c63563b10a40811e70181f0d8d
Dylan Nelson <Dylan.Nelson@nytimes.com>
2017-09-18 12:41:55 -0400

    Don't mutate contenteditables

--- src/content/index.js
+++ src/content/index.js
@@ -1,7 +1,19 @@
+function checkContenteditable(node) {
+  let n = node.parentElement;
+
+  while (n) {
+    if (n.getAttribute('contenteditable') !== null) return true;
+    n = n.parentElement;
+  }
+
+  return false;
+}
+
 const REGEX = new RegExp('New York Times', 'g');
 
 function processNode(node) {
   if (REGEX.test(node.textContent)) {
+    if (checkContenteditable(node) === true) return;
     const { textContent } = node;
     node.textContent = textContent.replace(REGEX, 'New York Fucking Times');
   }
